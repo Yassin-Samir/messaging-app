6816447cbfa1144e30c10d94a1e0b04db55324fc
yassin samir <yassinsam3@gmail.com>
2023-09-02 16:59:32 +0300

    fixing bugs

--- src/components/chatroom/chatroom.jsx
+++ src/components/chatroom/chatroom.jsx
@@ -4,18 +4,14 @@ import { useCollectionData } from "../../hooks/useCollectionData";
 import Form from "./Form";
 import ChatMessage from "./Chatmessage";
 const messagesRef = collection(db, "messages");
-const messagesQuery = query(
-  messagesRef,
-  limit(15),
-  orderBy("createdAt", "desc")
-);
+const messagesQuery = query(messagesRef, orderBy("createdAt"));
 function Chatroom() {
   const messages = useCollectionData(messagesQuery);
   return (
     <>
       <section>
         {messages &&
-          messages.reverse().map((i) => <ChatMessage {...i} key={i.docId} />)}
+          messages.map((i) => <ChatMessage {...i} key={i.docId} />)}
       </section>
       <Form messagesRef={messagesRef} />
     </>
